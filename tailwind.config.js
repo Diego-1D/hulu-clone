926112222549b4875ede9b27c101ba879935f8e9
Diego-1D <diego1d_fernandes@outlook.com>
2022-06-02 22:57:59 -0300

    module.exports tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      "3x1": "2000px"
+    },
+  },
+  plugins: [require('tailwind-scrollbar-hide')],
+}
